c2068c7312d00dc376da700876a34673e41ebd89
J-o-h-n-s <jaazpersonal@gmail.com>
2022-07-17 04:06:47 +0200

    Fixed cumulative profit and trade count

--- trading/paper/paperpt2.py
+++ trading/paper/paperpt2.py
@@ -11,31 +11,33 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 
 in_position = False
+trades = "0"
+cum_profit = "0"
 
 while True:
     df = pd.read_sql('ETHUSDT', engine)
     last_price = df.price.iloc[-1]
-    cum_profit = 1
-    trades = 1
+    cum_profit = float(cum_profit)
+    trades = int(trades)
     if not in_position:
-        if last_price > df.price.mean():
-            buyprice = last_price
-            in_position = True
-            print('Bought ETH @ ' +str(buyprice) + 'USDT at ' + current_time)
+        #if last_price > df.price.mean():
+        buyprice = last_price
+        in_position = True
+        print('Bought ETH @ ' +str(buyprice) + 'USDT at ' + current_time)
     if in_position:
         if last_price > 1.003 * buyprice or last_price < 0.997 * buyprice:
             sellprice = last_price
             profit = sellprice - buyprice
             in_position = False
             print('Sold ETH @ '+str(sellprice) + 'USDT at ' + current_time)
-            trades = trades + 1
-            print('Trades completed: ' + trades)
-            if profit > 0.00000000000000001:
-                print('Profit is: ' + profit + 'USDT')
-                cum_profit = cum_profit + profit
-                print('Cumulative profit:' + cum_profit + 'USDT')
+            trades = trades + (1)
+            print('Trades completed: ' + str(trades))
+            if profit > 0.00000000000000000000000000000000000000001:
+                print('Profit is: ' + str(profit) + 'USDT')
+                cum_profit = cum_profit + float(profit)
+                print('Cumulative profit:' + str(cum_profit) + 'USDT')
 
             else:
-                print('Loss is: ' + profit + 'USDT')
-                cum_profit = cum_profit - profit
-                print('Cumulatice profit:' + cum_profit + 'USDT')
+                print('Loss is: ' + str(profit) + 'USDT')
+                cum_profit = cum_profit + float(profit)
+                print('Cumulatice profit:' + str(cum_profit) + 'USDT')
