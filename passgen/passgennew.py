891e8591869bd3d70a5fd2bcbe5cad2d0c219663
J-o-h-n-s <jaazpersonal@gmail.com>
2022-07-21 02:45:34 +0200

    Work on issue print to file #2, now prints in line

--- passgen/passgennew.py
+++ passgen/passgennew.py
@@ -3,7 +3,7 @@ import random
 from os import system, name
 
 
-alphabets = list(string.ascii_letters)
+letters = list(string.ascii_letters)
 digits = list(string.digits)
 special_characters = list("!@#$%^&*()£")
 characters = list(string.ascii_letters + string.digits + '!@#$%^&*()£')
@@ -16,26 +16,19 @@ def clear():
         _ = system('clear')
 
 def generate_random_password():
+    
+    clear()
+
     length = int(input("Enter password length: "))
     amount = int(input('Enter amount of passwords: '))
 
-    alphabets_count = int(input("Enter letter count: "))
+    letters_count = int(input("Enter letter count: "))
     digits_count = int(input("Enter digits count: "))
     special_characters_count = int(input("Enter special characters count: "))
 
-#    if alphabets_count != int:
-#        print('Number of letters is not a number')
-#        exit()
-#    if digits_count != int:
-#        print('Number of digits is not a number')
-#        exit()
-#    if special_characters_count != int:
-#        print('Number of special characters is not a number')
-#        exit()
+    character_count = letters_count + digits_count + special_characters_count
 
-    character_count = alphabets_count + digits_count + special_characters_count
-
-    if character_count > length:
+    if character_count > length -1:
         print("Characters total count is greater than desired password length")
         exit()
 
@@ -50,38 +43,27 @@ def generate_random_password():
         for c in range(digits_count):
             password.append(random.choice(digits))
 
-        for c in range(alphabets_count):
-            password.append(random.choice(alphabets))
+        for c in range(letters_count):
+            password.append(random.choice(letters))
 
         for c in range(special_characters_count):
             password.append(random.choice(special_characters))
 
         if character_count < length:
             random.shuffle(characters)
-            for e in range(length - character_count):
+            for c in range(length - character_count):
                 password.append(random.choice(characters))
 
                 random.shuffle(password)
 
 
             if str(password) < str(length):
-                print('Error')
+                return()
             else:
                 print("".join(password))
 
 
-#            with open('Passowrds.txt', 'w') as file:
-#                for line in str(password):
-#                    file.write(line)
-#                    file.write('\n')
-
-#            file = open('Passwords.txt', 'w')
-#            str1 = repr(password)
-#            file.write('\n' + str1 + '\n')
-#            file.close
-#            f = open('Passwords.txt', 'r')
-#            if f .mode == 'r':
-#                contents=f.read
-#                contents
+    with open("Passwords.txt", "w") as f:
+        f.writelines(password)
 
 generate_random_password()
